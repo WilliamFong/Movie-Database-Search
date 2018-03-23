d3eb780e2afb1a42d11eeec73b2cc82fcc9d538e
Angie <sanchezg.angie@gmail.com>
2018-03-22 21:45:25 -0700

    Demand user for command

--- movie-app/cli.js
+++ movie-app/cli.js
@@ -1,4 +1,4 @@
-const 
+const
     app = require('./app'),
     yargs = require('yargs')
 
@@ -9,33 +9,33 @@ const flags = yargs.usage('$0: Usage <cmd> [options]')
                         builder: (yargs) => {
                             return yargs.option('p', {
                                 alias: 'person',
-                                describe: 'search moviedb base on person search'
+                                describe: 'search moviedb base on person search',
                             }).option('t', {
                                 alias: 'television',
                                 describe: 'search moviedb base on televsion search'
                             }).option('m',{
                                 alias: 'movies',
-                                describe: 'searcg noviedb base on movie search'
-                            } )
+                                describe: 'search moviedb base on movie search'
+                            })
                         },
                         //make sure user only search one at a time
                         //handler will use to see what to search if no options are chosen the use multi search
                         handler: (argv) => {
                             //when user just put search <arg>
-                            if(argv._.length >1){
+                            if (argv._.length > 1) {
                                 app.getSearch(argv._[1])
                             }
-                            //implemnt when user puts -tv -p -m 
-                            if(argv.t != null){
-
+                            //implement when user puts -tv -p -m
+                            if (argv.t != null) {
                                 app.getTvSearch(argv.t)
                                 console.log(argv.t)
                             }
-                            if(argv.p != null) {
+                            if (argv.p != null) {
                                 app.getPersonSearch(argv.p)
                             }
                             console.log(argv)
                         }
                     })
+                    .demand(1, 'Please enter command.')
                     .help('help')
                     .argv
