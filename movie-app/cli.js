ae9b123131939d86fcb0bed376ffe275987aad8c
DanielrR0m0 <drdanielromo@gmail.com>
2018-03-23 19:10:49 -0700

    update

--- movie-app/cli.js
+++ movie-app/cli.js
@@ -29,13 +29,16 @@ const flags = yargs.usage('$0: Usage <cmd> [options]')
                             }
                             //implemnt when user puts -tv -p -m 
                             else if(argv.t != null & argv.p == null & argv.m ==null & argv.t != true){
-                                app.getTvSearch(`${argv.t}${argv._.slice(1).join('+')}`)
+                                console.log(`${argv.t}${argv._.slice(1).join('+')}`);
+                                app.getTvSearch(`${argv.t}+${argv._.slice(1).join('+')}`)
                             }
                             else if(argv.p != null & argv.t == null & argv.m ==null & argv.p !=true){
-                                app.getPersonSearch(`${argv.p}${argv._.slice(1).join('+')}`)
+                                console.log(`${argv.p}+${argv._.slice(1).join('+')}`);
+                                app.getPersonSearch(`${argv.p}+${argv._.slice(1).join('+')}`)
                             }
                             else if(argv.m != null & argv.p == null & argv.t == null & argv.m !=true){
-                                app.getMovieSearch(`${argv.m}${argv._.slice(1).join('+')}`)
+                                console.log(`${argv.m}+${argv._.slice(1).join('+')}`);
+                                app.getMovieSearch(`${argv.m}+${argv._.slice(1).join('+')}`)
                             }else{
                                 yargs.showHelp()
                             }
